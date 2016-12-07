5e0815d4c30e82d68f615eebd5ed5b4156a9ed24
denisslapcenko <dlapcenko@salmon.com>
2016-12-07 23:33:36 +0000

    .asset files

---
+++ Assets/Scripts/Controller/SceneLoader.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+using Zenject;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+namespace ToughLife
+{
+	public class SceneLoader : MonoBehaviour
+	{
+		GameSceneSignal _sceneSignal;
+
+		[Inject]
+		public void Construct (GameSceneSignal sceneSignal)
+		{
+			_sceneSignal = sceneSignal;
+			_sceneSignal += GameScene;
+		}
+
+		void GameScene(string sceneName)
+		{
+			Debug.Log("SCENE LOADINK = "+sceneName);
+		}
+	}
+}
+
