9ccbb75e57c630ae5b50ec38e17aaa32aa4a2fab
denisslapcenko <dlapcenko@salmon.com>
2016-12-07 23:32:49 +0000

    gitignore update (allows .asset)

---
+++ Assets/Scripts/Signals/GameSceneSignal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Zenject;
+using UnityEngine;
+
+namespace ToughLife
+{
+	public static class GameStateSignals
+	{
+		public class Bootstrap : Signal<Bootstrap>
+		{
+		}
+		public class Main : Signal<Main>
+		{
+		}		
+		public class Session : Signal<Session>
+		{
+		}
+		public class Gameover : Signal<Gameover>
+		{
+			
+		}
+	}
+}
+
